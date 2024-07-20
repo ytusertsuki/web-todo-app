a272a14ce8e5058c304d4416cd634c1656e58344
ytusertsuki <yuto.engin762@gmail.com>
2024-07-20 15:44:04 +0900

    second commit

---
+++ app/database/migrations/2024_07_19_073538_create_todo_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Users;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('todo_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id') // 外部キーを追加
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->text('title');
+            $table->boolean('is_done')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     * 
+     * @return void
+     */
+
+    public function down(): 
+    {
+        Schema::dropIfExists('todo_items');
+    }
+};
